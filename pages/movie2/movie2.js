31588404dfbd786e31c4da2a464f28c6fc3e7b29
ParkerLLF <1003967226@QQ.COM>
2019-06-27 19:43:17 +0800

    地图

--- pages/movie2/movie2.js
+++ pages/movie2/movie2.js
@@ -96,7 +96,21 @@ Page({
             url: '../movie/movie',
         })
     },
-
+    // 地图功能
+    map: function() {
+        wx.getLocation({ //获取当前经纬度
+            type: 'wgs84', //返回可以用于wx.openLocation的经纬度，官方提示bug: iOS 6.3.30 type 参数不生效，只会返回 wgs84 类型的坐标信息  
+            success: function(res) {
+                wx.openLocation({ //使用微信内置地图查看位置。
+                    latitude: 30.002353, //要去的纬度-地址
+                    longitude: 104.661953, //要去的经度-地址
+                    
+                    name: "忠义镇",
+                    address: '王二溪',
+                })
+            }
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
