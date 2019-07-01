c2f89072c4302cf1a5d198602ac9b875110302b2
ParkerLLF <1003967226@QQ.COM>
2019-07-01 17:30:06 +0800

    新增服务器模块

---
+++ 模拟服务器 链接数据库/model/ImgurlsModel.js
@@ -0,0 +1,30 @@
+let BaseSql = require("./BaseSql");
+class ImgurlsModel extends BaseSql {
+
+    constructor() {
+        super();
+    }
+
+    getImgurls(call) {
+        //3,编写sql语句
+        let sql = 'SELECT * FROM imgUrls';
+
+        //4,进行查询操作
+        /**
+         *query，mysql语句执行的方法
+         * 1，userAddSql编写的sql语句
+         * 2，function (err, result)，回调函数，err当执行错误时，回传一个err值，当执行成功时，传回result
+         */
+        this.connection.query(sql, function (err, result) {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
+            call(result);
+
+        });
+        //5,连接结束
+        //this.connection.end();
+    }
+}
+module.exports = ImgurlsModel;
